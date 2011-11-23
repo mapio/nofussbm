c303debbf80c538fbbea94afe39f5b5d4fbcbc76
Massimo Santini <santini@dsi.unimi.it>
2011-11-23 13:44:18 +0100

    Added tests and PUT stuff

--- server/nofussbm.py
+++ server/nofussbm.py
@@ -4,12 +4,14 @@ from base64 import b64encode, b64decode
 from functools import wraps
 from json import dumps
 
-from flask import Flask, make_response, request, g, jsonify
+from flask import Flask, make_response, request, g
 
 from pymongo import Connection
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.secret_key = 'a well kept secret'
+
 
 def new_key( email ):
 	return b64encode( '{0}:{1}'.format( email, hmac.new( app.secret_key, email, sha1 ).hexdigest() ) )
@@ -24,6 +26,16 @@ def check_key( key ):
 	else:
 		return None
 
+def textify( text ):
+	response = make_response( text + '\n' )
+	response.headers[ 'Content-Type' ] = 'text/plain; charset=UTF-8'
+	return response
+
+def myjsonify( data ):
+	response = make_response( dumps( data, indent = 4, ensure_ascii = False ) + '\n' )
+	response.headers[ 'Content-Type' ] = 'application/json; charset=UTF-8'
+	return response
+	
 @app.before_request
 def before_request():
 	g.conn = Connection( 'mongodb://test:pippo@dbh45.mongolab.com:27457/test' )
@@ -36,36 +48,61 @@ def teardown_request( exception ):
 def key_required( f ):
 	@wraps( f )
 	def _f( *args, **kwargs ):
- 		g.email  = check_key( request.headers[ 'X-Nofussbm' ] )
-		return f( *args, **kwargs )
+		try:
+ 			email  = check_key( request.headers[ 'X-Nofussbm-Key' ] )
+		except KeyError:
+			email = None
+		if email:
+			g.email = email
+			return f( *args, **kwargs )
+		else:
+			response = make_response( 'Missing HTTP header X-Nofussbm-Key', 403 )
+			response.headers[ 'Content-Type' ] = 'text/plain'
+			return response
 	return _f
 
-@app.route('/', methods = [ 'POST' ] )
-@key_required
-def post():
-	if g.email:
-		data = request.json
-		data[ 'email' ] = g.email
-		result = 'Bookmark id: {0}'.format( g.db.insert( data ) )
-	else:
-		result = 'Invalid API key'
-	response = make_response( result )
-	response.headers[ 'Content.type' ] = 'text/plain'
-	return response
 
-@app.route('/', methods = [ 'GET' ] )
-@key_required
-def get():
-	result = [ { 'id': str( bm[ '_id' ] ), 'url': bm[ 'url'], 'title': bm[ 'title' ], 'tags': bm[ 'tags' ] } for bm in g.db.find( { 'email': g.email } ) ]
-	response = make_response( dumps( result ) )
-	response.headers[ 'Content.type' ] = 'application/json'
-	return response
+# Public "views"
 
 @app.route('/key/<email>')
 def key( email ):
-	response = make_response( new_key( email ) + '\n' )
-	response.headers[ 'Content.type' ] = 'text/plain'
-	return response
+	return textify( new_key( email ) )
+
+
+# API "views"
+
+API_PREFIX = '/api/v1'
+
+@app.route( API_PREFIX + '/', methods = [ 'PUT' ] )
+@key_required
+def put():
+	result = { 'error': [], 'updated': [], 'ignored': [] }
+	for bm in request.json:
+		_id = ObjectId( bm[ 'id' ] )
+		del bm[ 'id' ]
+		bm[ 'email' ] = g.email
+		ret = g.db.update( { '_id': _id  }, { '$set': bm }, safe = True )
+		result[ 'error' if ret[ 'err' ] else 'updated' if ret[ 'updatedExisting' ] else 'ignored' ].append( str( _id ) )
+	return myjsonify( result )
+
+@app.route( API_PREFIX + '/', methods = [ 'POST' ] )
+@key_required
+def post():
+	data = request.json
+	data[ 'email' ] = g.email
+	result = 'Bookmark id: {0}'.format( g.db.insert( data ) )
+	return textify( result )
+
+@app.route( API_PREFIX + '/', methods = [ 'GET' ] )
+@key_required
+def get():
+	result = []
+	for bm in g.db.find( { 'email': g.email } ):
+		bm[ 'id' ] = str( bm[ '_id' ] )
+		del bm[ '_id' ]
+		del bm[ 'email' ]
+		result.append( bm )
+	return myjsonify( result )
 
 if __name__ == "__main__":
 	app.run( debug = True )
