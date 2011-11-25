2e5fb4e841f21787745d09ccc2f9b17793d0884a
Massimo Santini <santini@dsi.unimi.it>
2011-11-25 01:19:23 +0100

    Added GPL

---
+++ README.rst
@@ -0,0 +1,13 @@
+No Fuss Bookmarks
+=================
+
+No Fuss Bookmarks is a very simple **software** and **service** to store bookmarks especially designed for hackers (that don't need
+fancy interfaces, but **nice API**).
+
+The software is a simple RESTful server, written using `Flask <http://flask.pocoo.org/>`_, providing a CRUD interface to a `mongoDB
+<http://www.mongodb.org/>`_ store, plus a very basic Google Chrome extension to submit bookmarks.
+
+The service is just an incarnation of such software hosted by `heroku <http://www.heroku.com/>`_ and `mongolab
+<http://mongolab.com>`_ that you can freely use just submitting your email below to obtain your API key.
+
+For more details, please visit the `service signup page <http://nofussbm.herokuapp.com/static/signup.html>`_.
--- server/nofussbm/helpers.py
+++ server/nofussbm/helpers.py
@@ -1,3 +1,20 @@
+# Copyright 2011, Massimo Santini <santini@dsi.unimi.it>
+# 
+# This file is part of "No Fuss Bookmarks".
+# 
+# "No Fuss Bookmarks" is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+# 
+# "No Fuss Bookmarks" is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# "No Fuss Bookmarks". If not, see <http://www.gnu.org/licenses/>.
+
 from datetime import datetime
 import gzip
 from json import JSONEncoder, JSONDecoder
