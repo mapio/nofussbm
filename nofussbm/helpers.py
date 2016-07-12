0470fa050439bb0499249016050c7b17a9a0405c
Massimo Santini <santini@di.unimi.it>
2016-07-12 16:52:01 +0200

    Added support for IDs

--- nofussbm/helpers.py
+++ nofussbm/helpers.py
@@ -1,17 +1,17 @@
 # Copyright 2011, Massimo Santini <santini@dsi.unimi.it>
-# 
+#
 # This file is part of "No Fuss Bookmarks".
-# 
+#
 # "No Fuss Bookmarks" is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option) any
 # later version.
-# 
+#
 # "No Fuss Bookmarks" is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
 # details.
-# 
+#
 # You should have received a copy of the GNU General Public License along with
 # "No Fuss Bookmarks". If not, see <http://www.gnu.org/licenses/>.
 
@@ -39,10 +39,12 @@ def to_id( id_as_str ):
 def query_from_dict( email, dct ):
 	query = { 'email': email }
 	if not dct: return query
-	if 'tags' in dct: 
+	if 'id' in dct:
+		query[ '_id' ] = ObjectId( dct[ 'id'] )
+	if 'tags' in dct:
 		tags = map( lambda _: _.strip(), dct[ 'tags' ].split( ',' ) )
 		query[ 'tags' ] = { '$all': tags }
-	if 'title' in dct: 
+	if 'title' in dct:
 		query[ 'title' ] = { '$regex': dct[ 'title' ], '$options': 'i' }
 	return query
 
@@ -50,7 +52,7 @@ def setup_json( json ):
 
 	def object_hook( dct ):
 		res = {}
-		for key, value in dct.items(): 
+		for key, value in dct.items():
 			if key not in ALLOWED_KEYS: continue
 			if key == 'id': res[ 'id' ] = to_id( value )
 			elif key == 'tags': res[ 'tags' ] = map( lambda _: _.strip(), value.split( ',' ) )
@@ -70,16 +72,16 @@ def setup_json( json ):
 			if isinstance( obj, ObjectId ):
 				return str( obj )
 			return JSONEncoder.default( self, obj )
-	
+
 	prev_dumps = json.dumps
 	prev_loads = json.loads
-	
+
 	def _dumps( *args, **kwargs ):
 		kwargs.update( { 'cls':  Encoder } )
 		return prev_dumps( *args, **kwargs )
 	def _loads( *args, **kwargs ):
 		return prev_loads( *args, object_hook = object_hook, **kwargs )
-	
+
 	json.dumps = _dumps
 	json.loads = _loads
 
