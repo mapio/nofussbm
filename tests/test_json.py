bbe2eb53508b3ba41b8d1758444a30e793c60dce
devster31 <devster31@gmail.com>
2016-07-19 02:46:06 +0200

    Add tests for the json encoder and decoder

---
+++ tests/test_json.py
@@ -0,0 +1,51 @@
+import unittest
+import nofussbm
+import flask
+import datetime
+
+from bson import ObjectId
+
+
+class NofussbmJSONTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = nofussbm.app
+        self.app.config['TESTING'] = True
+        self.client = nofussbm.app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def testJson(self):
+        expected_load = [{
+            u'date-modified': datetime.datetime(2016, 7, 16, 0, 0, 43, 237000),
+            u'title': u'Google',
+            u'url': u'https://google.com',
+            u'tags': [u'google', u'search engine'],
+            u'id': ObjectId('5789792b19f4cb77cc3be929'),
+            u'date-added': datetime.datetime(2016, 7, 16, 0, 0, 43, 237000)
+        }]
+        to_load = ('[{'
+                   '"date-added": "2016-07-16 00:00:43.237000", '
+                   '"date-modified": "2016-07-16 00:00:43.237000", '
+                   '"id": "5789792b19f4cb77cc3be929", '
+                   '"tags": "google,search engine", '
+                   '"title": "Google", '
+                   '"url": "https://google.com"'
+                   '}]')
+        expected_dump = ('[{'
+                         '"date-added": "2016-07-16 00:00:43.237000", '
+                         '"date-modified": "2016-07-16 00:00:43.237000", '
+                         '"id": "5789792b19f4cb77cc3be929", '
+                         '"tags": ["google", "search engine"], '
+                         '"title": "Google", '
+                         '"url": "https://google.com"'
+                         '}]')
+        with self.app.app_context():
+            load = flask.json.loads(to_load)
+            dump = flask.json.dumps(expected_load)
+            self.assertEqual(load, expected_load)
+            self.assertEqual(dump, expected_dump)
+
+if __name__ == '__main__':
+    unittest.main()
